38b879eab86b511c5a96c851beb03407d3b1206b
Bobbychiken <bobbychiken@yahoo.com>
2026-09-15 22:17:55 -0600

    saving to ammend

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+    anchor_text = TextNode("some anchor text", TextType.LINK , "https://www.boot.dev")
+    print(anchor_text)
+
+if __name__ == "__main__":
+    main()
